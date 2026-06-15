2f8b31462c8c2a3184933ecd14b4dc41963b8c87
mrbeers22322 <mrbeers2232@gmail.com>
2026-06-14 23:50:33 -0600

    Use number keyboard for CP entry
    - Updated ManualPokemonEntryForm CP field to use numeric keyboard input
    - Added KeyboardOptions with KeyboardType.Number
    - Improved manual Pokemon entry experience on Android devices

--- android/app/src/main/java/com/mattbeers/pokebrain/ui/components/ManualPokemonEntryForm.kt
+++ android/app/src/main/java/com/mattbeers/pokebrain/ui/components/ManualPokemonEntryForm.kt
@@ -3,6 +3,7 @@ package com.mattbeers.pokebrain.ui.components
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -10,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -48,6 +50,9 @@ fun ManualPokemonEntryForm(
         label = {
             Text("CP")
         },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number
+        ),
         modifier = Modifier.fillMaxWidth()
     )
 
